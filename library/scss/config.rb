95a5c5505af19ee57c1c26e203ada19f85f7a825
Jonathan Roth <jwayneroth@gmail.com>
2015-03-22 18:40:32 -0400

    init

---
+++ library/scss/config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "../css"
+sass_dir = "./"
+images_dir = "../images"
+javascripts_dir = "../js"
+environment = :development
+relative_assets = true
+output_style = :expanded
+# output_style = :compressed
+line_comments = true
+# don't touch this
+preferred_syntax = :scss
---
+++ page.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+								<header class="article-header">
+
+									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+
+									<p class="byline vcard">
+										<?php printf( __( 'Posted', 'bonestheme').' <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> '.__( 'by',  'bonestheme').' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
+									</p>
+
+								</header> <?php // end article header ?>
+
+								<section class="entry-content cf" itemprop="articleBody">
+									<?php
+										// the content (pretty self explanatory huh)
+										the_content();
+									?>
+								</section> <?php // end article section ?>
+
+								<footer class="article-footer cf">
+
+								</footer>
+
+								<?php //comments_template(); ?>
+
+							</article>
+
+							<?php endwhile; endif; ?>
+
+						</main>
+
+						<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>

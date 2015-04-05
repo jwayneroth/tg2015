1c6a4f98965b83da39b6ba04bdeeb97b7029bb0e
Jonathan Roth <jwayneroth@gmail.com>
2015-04-04 20:28:40 -0400

    thumb size and colors

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ sidebars, comments, ect.
 //////////////////////////////
 
 add_image_size( 'project-slide', 800, 800 );
-add_image_size( 'project-thumb', 260, 175, true );
+add_image_size( 'project-thumb', 260, 260, true );//260, 175, true );
 add_image_size( 'home-banner', 380, 516, true );
 
 function tg_page_scripts() {
